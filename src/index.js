ddf8fbb4ee886d26db70450146143984a8f427e6
AndrewKytsan <andrew.kytsan@gmail.com>
2021-08-29 16:00:37 +0300

    hw -10

---
+++ src/index.js
@@ -0,0 +1,43 @@
+
+import refs from './refs.js';
+import menu from './menu.json'
+import foodCard from './templates/food-card.hbs'
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+if (localStorage.getItem('Theme') === null) {
+  refs.body.classList.add('light-theme')
+ }
+
+refs.themeSwitchToggle.addEventListener('change', changeTheme)
+ 
+function changeTheme() {
+  if (refs.themeSwitchToggle.checked === true) {
+    refs.body.classList.remove('light-theme')
+    refs.body.classList.add('dark-theme')
+    localStorage.removeItem('Theme')
+    localStorage.setItem('Theme', 'DARK')
+  }
+  else {
+    refs.body.classList.remove('dark-theme')
+    refs.body.classList.add('light-theme')
+    localStorage.removeItem('Theme')
+    localStorage.setItem('Theme', 'LIGHT')
+
+  }
+}
+
+if (localStorage.getItem('Theme') === 'DARK') {
+  refs.themeSwitchToggle.checked = true;
+  refs.body.classList.add('dark-theme')
+}
+
+
+
+function CreateMenu(menu) {
+  return menu.map(foodCard).join ('')
+}
+refs.menu.insertAdjacentHTML('beforeend',CreateMenu(menu))
---
+++ src/refs.js
@@ -0,0 +1,6 @@
+export  default{
+  body: document.querySelector('body'),
+  themeSwitchToggle: document.querySelector('.theme-switch__toggle'),
+  menu: document.querySelector('.menu')
+};
+
